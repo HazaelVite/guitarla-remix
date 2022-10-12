99a09bfcdb435e4d12a3d71128c5037456626025
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2022-10-11 20:33:33 -0500

    Creando pagina de nosotros

--- app/routes/nosotros.jsx
+++ app/routes/nosotros.jsx
@@ -6,9 +6,28 @@ export function meta() {
   )
 }
 
+import imagen from '../../public/img/nosotros.jpg'
+
 function Nosotros() {
   return (
-    <div>nosotros</div>
+    <main className="contenedor nosotros">
+      <h2 className="heading">Nosotros</h2>
+      <div className="contenido">
+        <img src={imagen} alt="Sobre Nosotros" />
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores inventore, nam minima fugiat atque,
+            repellat quidem suscipit aliquid in necessitatibus deleniti odit, maxime hic ipsa neque.
+            Ipsum at minima obcaecati?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores inventore, nam minima fugiat atque,
+            repellat quidem suscipit aliquid in necessitatibus deleniti odit, maxime hic ipsa neque.
+            Ipsum at minima obcaecati?
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
 
