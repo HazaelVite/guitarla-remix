518b276b017c517ae469a4b9fa1eee8f54a3f3e8
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2022-10-11 23:25:08 -0500

    Aplicando CSS al listado de guitarras

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,6 +1,23 @@
 import { useLoaderData } from '@remix-run/react'
 import Guitarra from '~/components/guitarra'
 import { getGuitarras } from '~/models/guitarras.server'
+import styles from '~/styles/guitarras.css'
+
+export function meta() {
+  return {
+    title: 'GuitarLA - Tienda de Guitarras',
+    description: 'GuitarLA - Nuestra colección de guitarras'
+  }
+}
+//  Hoja de estilos
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
 
 export async function loader() {
   const guitarras = await getGuitarras()
