e3944499ef15dc83e24504f80bbe0722f58f6e7f
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2022-10-11 23:07:50 -0500

    Mostrando la información de las guitarras

--- app/components/guitarra.jsx
+++ app/components/guitarra.jsx
@@ -1,6 +1,20 @@
-function Guitarra() {
+import { Link } from "@remix-run/react"
+
+function Guitarra({guitarra}) {
+  const { descripcion, imagen, precio, url, nombre } = guitarra
   return (
-    <div>Guitarra</div>
+    <div className="guitarra">
+      <img src={imagen.data.attributes.formats.medium.url} alt={ nombre } />
+      <div className="contenido">
+        <h3>{ nombre }</h3>
+        <p className="descripcion">{ descripcion }</p>
+        <p className="precio">$ { precio}</p>
+
+        <Link className="enlace" to={`/guitarras/${url}`}>
+          Ver Producto
+        </Link>
+      </div>
+    </div>
   )
 }
 
