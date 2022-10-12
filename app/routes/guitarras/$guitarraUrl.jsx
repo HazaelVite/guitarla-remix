8d4a9409fae7922b90e8a2e57e9be8c74f680db8
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2022-10-11 23:43:13 -0500

    Mostrando la información de la guitarra

--- app/routes/guitarras/$guitarraUrl.jsx
+++ app/routes/guitarras/$guitarraUrl.jsx
@@ -2,6 +2,16 @@
 // Siempre iniciar el nombre del archivo con $
 import { useLoaderData } from "@remix-run/react"
 import { getGuitarra } from "~/models/guitarras.server"
+import styles from '~/styles/guitarras.css'
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
 
 export async function loader({params}) {
   const { guitarraUrl } = params
@@ -11,9 +21,20 @@ export async function loader({params}) {
 
 function Guitarra() {
   const guitarra = useLoaderData()
-  
+
+  const { nombre, descripcion, imagen, precio } = guitarra.data[0]?.attributes
+
   return (
-    <div>Guitarra</div>
+    <main className="contenedor guitarra">
+      <img src={imagen.data.attributes.url} alt={nombre} className="imagen" />
+      <div className="contenido">
+        <h3>{nombre}</h3>
+        <p className="text">
+          {descripcion}
+        </p>
+        <p className="precio">$ {precio}</p>
+      </div>
+    </main>
   )
 }
 
