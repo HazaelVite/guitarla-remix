e203a4a0f2584c988c059aac82c4e28ea6e423c3
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2022-10-11 23:36:28 -0500

    Filtrar guitarras por URL desde Strapi

--- app/routes/guitarras/$guitarraUrl.jsx
+++ app/routes/guitarras/$guitarraUrl.jsx
@@ -1,12 +1,17 @@
 // Componente para hacer rutas dinamicas
 // Siempre iniciar el nombre del archivo con $
+import { useLoaderData } from "@remix-run/react"
+import { getGuitarra } from "~/models/guitarras.server"
 
 export async function loader({params}) {
   const { guitarraUrl } = params
-  return {}
+  const guitarra = await getGuitarra(guitarraUrl)
+  return guitarra
 }
 
 function Guitarra() {
+  const guitarra = useLoaderData()
+  
   return (
     <div>Guitarra</div>
   )
