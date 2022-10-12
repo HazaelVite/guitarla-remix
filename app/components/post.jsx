4509d87f33d3ee54513b861a9c24e3da4dbd43a7
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2022-10-12 00:11:06 -0500

    Obtener entradas desde strapy para el blog

---
+++ app/components/post.jsx
@@ -0,0 +1,19 @@
+import { Link } from "@remix-run/react"
+
+
+export default function Post({post}) {
+
+  const { contenido, imagen, titulo, url, createdAt } = post
+  return (
+    <article className="post">
+      <img className="imagen" src={imagen.data.attributes.formats.small.url} alt={titulo} />
+      <div className="contenido">
+        <h3>{ titulo }</h3>
+        <p className="fecha">{ createdAt }</p>
+        <p className="resumen">{ contenido }</p>
+        <Link className="enlace" to={`/posts/${url}`}>Leer Entrada</Link>
+        
+      </div>
+    </article>
+  )
+}
