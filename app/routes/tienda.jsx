be1466cbb13833623a2d1205f0fd1936bf59e574
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2022-10-11 20:56:08 -0500

    Creando un Modelo que se ejecute en el servidor

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,11 +1,9 @@
+import { getGuitarras } from '~/models/guitarras.server'
+
+
 export async function loader() {
-  const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate+imagen`)
-  const resultado = await respuesta.json
-
-  console.log(resuultado);
-
-
-  return {}
+  const guitarras = await getGuitarras()
+  return guitarras
 }
 
 function Tienda() {
