5a98038dfc85f0628020dfc150ac0f041d9794a4
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2022-10-18 23:11:45 -0500

    Remix Avanzado - Nested Routes para Blog

--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,17 +1,5 @@
-import { useLoaderData } from "@remix-run/react"
-import ListadoPosts from "~/components/listadoPosts"
-import Post from "~/components/post"
-import { getPosts } from "~/models/posts.server"
+import { Outlet } from "@remix-run/react"
 import styles from '~/styles/blog.css'
-
-export function meta() {
-  return (
-    {
-      title: 'GuitarLA - Nuestro Blog',
-      description: 'Blog de música y venta de guitarras'
-    }
-  )
-}
 
 export function links() {
   return [
@@ -22,16 +10,10 @@ export function links() {
   ]
 }
 
-export async function loader() {
-  const posts = await getPosts()
-  return posts.data
-}
-
 function Blog() {
-  const posts = useLoaderData()
   return (
     <main className="contenedor">
-      <ListadoPosts posts={posts} />
+      <Outlet />
     </main>
   )
 }
--- app/routes/blog/$postUrl.jsx
+++ app/routes/blog/$postUrl.jsx
@@ -2,12 +2,11 @@
 import { useLoaderData } from "@remix-run/react";
 import { getPost } from "~/models/posts.server";
 import { formatearFecha } from "~/utils/helpers"
-import styles from '~/styles/blog.css'
 
 export async function loader({ params }) {
   const { postUrl } = params
   const post = await getPost(postUrl)
-  if(post.data.length === 0) {
+  if (post.data.length === 0) {
     throw new Response('', {
       status: 404,
       statusText: 'Entrada no encontrada'
@@ -16,8 +15,8 @@ export async function loader({ params }) {
   return post
 }
 
-export function meta({data}) {
-  if(!data) {
+export function meta({ data }) {
+  if (!data) {
     return {
       title: 'GuitarLA - Entrada no encontrada'
     }
@@ -27,27 +26,18 @@ export function meta({data}) {
   }
 }
 
-export function links() {
-  return [
-    {
-      rel: 'stylesheet',
-      href: styles
-    }
-  ]
-}
-
-export default function Post () {
+export default function Post() {
   const post = useLoaderData()
 
   const { contenido, imagen, titulo, url, createdAt } = post.data[0]?.attributes
 
   return (
-    <article className="contenedor post mt-3">
+    <article className="post mt-3">
       <img className="imagen" src={imagen.data.attributes.url} alt={titulo} />
       <div className="contenido">
-        <h3>{ titulo }</h3>
-        <p className="fecha">{ formatearFecha(createdAt) }</p>
-        <p className="texto">{ contenido }</p>        
+        <h3>{titulo}</h3>
+        <p className="fecha">{formatearFecha(createdAt)}</p>
+        <p className="texto">{contenido}</p>
       </div>
     </article>
   )
