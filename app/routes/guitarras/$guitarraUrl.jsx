6d2634325769b1e60fefe777364e12b61a4e19fa
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2022-10-24 22:21:21 -0500

    Añadiendo la Opción de Añadir al Carrito en la URL de Guitarra

--- app/routes/guitarras/$guitarraUrl.jsx
+++ app/routes/guitarras/$guitarraUrl.jsx
@@ -3,11 +3,11 @@
 import { useLoaderData } from "@remix-run/react"
 import { getGuitarra } from "~/models/guitarras.server"
 
-export async function loader({params}) {
+export async function loader({ params }) {
   const { guitarraUrl } = params
   const guitarra = await getGuitarra(guitarraUrl)
 
-  if(guitarra.data.length === 0) {
+  if (guitarra.data.length === 0) {
     throw new Response('', {
       status: 404,
       statusText: 'Guitarra no encontrada'
@@ -16,8 +16,8 @@ export async function loader({params}) {
   return guitarra
 }
 
-export function meta({data}) {
-  if(!data) {
+export function meta({ data }) {
+  if (!data) {
     return {
       title: 'GuitarLA - Guitarra no encontrada'
     }
@@ -41,6 +41,20 @@ function Guitarra() {
           {descripcion}
         </p>
         <p className="precio">$ {precio}</p>
+        <form className="formulario">
+          <label htmlFor="cantidad">Cantidad</label>
+
+          <select name="cantidad" id="cantidad">
+            <option value="">-- Seleccione --</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+
+          <input type="submit"  value="Agregar al carrito" />
+        </form>
       </div>
     </div>
   )
