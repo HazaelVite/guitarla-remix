45cd4660a33370c82240a839c13403a04af7e909
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2022-10-24 22:13:58 -0500

    Añadiendo el contenido y css

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,7 +1,39 @@
+import styles from '~/styles/carrito.css'
 
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
+
+export function meta() {
+  return {
+    title: 'GuitarLA - Carrito de Compras', 
+    description: 'Venta de guitarras, musica, blog, carrito de compras, tienda'
+  }
+}
 
 export default function Carrito() {
   return (
-    <div>carrito</div>
+    <main className="contenedor">
+      <h1 className="heading">Carrito de compras</h1>
+
+      <div className="contenido">
+        <div className="carrito">
+          <h2>Articulos</h2>
+        </div>
+
+        <aside className="resumen">
+          <h3>Resumen del pedido</h3>
+          <p>Total a pagar</p>
+        </aside>
+
+      </div>
+
+
+    </main>
   )
 }
